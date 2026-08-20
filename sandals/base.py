93b4f7b70fe26395fbef3707e42a7fa88af11f19
n8jhj <nathaniel-j-jones@proton.me>
2026-08-19 23:16:33 -0700

    Revert "Introduce BindAttr class to capture default values for empty()"
    This reverts commit c73e6d5fb129e4f41d7b88dce33ee66daf63e960.
    
    I realized I don't want to give up the simplicity of just having class
    annotations. This keeps the type checker happy and makes it possible to
    deal with DataClasses that have DataClass attributes. Really, we should
    just have predetermined default values for POD types.

--- sandals/base.py
+++ sandals/base.py
@@ -1,10 +1,1 @@
-import typing as t
-
-
-class BindAttr:
-    def __init__[T: t.Any](self, type_: type[T], default_value: T) -> None:
-        self.type = type_
-        self.default_value = default_value
-
-
 class BindBase: ...
