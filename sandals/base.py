c73e6d5fb129e4f41d7b88dce33ee66daf63e960
n8jhj <nathaniel-j-jones@proton.me>
2026-08-19 23:03:28 -0700

    Introduce BindAttr class to capture default values for empty()

--- sandals/base.py
+++ sandals/base.py
@@ -1,1 +1,10 @@
+import typing as t
+
+
+class BindAttr:
+    def __init__[T: t.Any](self, type_: type[T], default_value: T) -> None:
+        self.type = type_
+        self.default_value = default_value
+
+
 class BindBase: ...
